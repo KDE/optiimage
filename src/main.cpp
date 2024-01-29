f16880bb136a01ee4f1c74e246d2d022be16b313
Carl Schwan <carl@carlschwan.eu>
2024-01-30 00:16:39 +0100

    Remove ifdef for qt5

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,6 @@
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
     QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("optiimage");
 
